bcce8ed9925517f9e088fc6caa4bdf2b24c53831
Anuoluwapo Gabriel Oyetibo <54692909+Aoyetibo557@users.noreply.github.com>
2023-10-10 20:13:04 -0400

    new welcome page for the api

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const controllers = require("./controllers/index");
 const taskRoutes = require("./routes/task.routes");
 const boardRoutes = require("./routes/board.routes");
 const userRoutes = require("./routes/auth.routes");
+const notificationRoutes = require("./routes/notification.routes");
 require("dotenv").config();
 
 const app = express();
 
 var corsOption = {
-  origin: [
-    "https://task-manager-api-dun.vercel.app",
-    "https://task-manager-client-nu.vercel.app",
-    "http://localhost:9090",
-    "http://localhost:3000",
-  ],
-  credentials: true,
-  optionSuccessStatus: 200,
+    origin: [
+        "https://task-manager-api-dun.vercel.app",
+        "https://task-manager-client-nu.vercel.app",
+        "http://localhost:9090",
+        "http://localhost:3000",
+    ],
+    credentials: true,
+    optionSuccessStatus: 200,
 };
 
 app.use(cors(corsOption));
@@ -24,32 +25,40 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Middleware that lifts the cors restriction for routing from a diffrent url to the server url
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, content-type, Authorization"
-  );
-  next();
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With, content-type, Authorization"
+    );
+    next();
 });
+
+const getRoutesForController = (controller) => {
+    return textRoutes[controller] || [];
+};
+
+// for you Anu, create a dummp profile so the api can be tested by those who go to look at it. make sure to put it up. Also build an html page that can be passed with all these as opposed to the json.
 
 // simple route
 app.get("/", (req, res) => {
-  const message =
-    "Welcome to Tassker's REST API. To get started, please visit /api/{controller}/{route} to see the API's endpoints.";
-  res.json({ message: message });
+    const fs = require("fs");
+    const htmlContent = fs.readFileSync("./welcome.html", "utf8");
+
+    res.send(htmlContent);
 });
 
 app.use("/api", controllers);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/boards", boardRoutes);
 app.use("/api/auth", userRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 const PORT = process.env.PORT || 9090;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
 
 // if (PORT) {
